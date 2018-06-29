9a7c23be4c80df36bc705bf6a59cdd6356bc73a3
mzandrew <mza@phys.hawaii.edu>
2018-06-29 11:25:20 -1000

    fixed bug; it works now

--- verilog/src/mza-test003.double-dabble.v
+++ verilog/src/mza-test003.double-dabble.v
@@ -16,27 +16,25 @@ module hex2bcd(input clock, input reset, input [7:0] hex_in, output reg [11:0] b
 			bcd_out <= 0;
 			bit_counter <= 0;
 		end else begin
-//			bcd_out <= bit_counter;
-//			bcd_out[7:0] <= hex_in;
-			if (bit_counter == 0) begin
-				bit_counter <= (input_size_in_bits << 1) + 1; // should be 17
+			if (bit_counter == 0) begin // bit_counter is 0
+				bit_counter <= (input_size_in_bits << 1); // should be 16
 				shift_register[input_size_in_bits-1:0] <= hex_in; // lower part
 				shift_register[total_size_in_bits-1:input_size_in_bits] <= 0; // upper part
-			end else if (bit_counter == 1) begin
+			end else if (bit_counter == 1) begin // bit_counter is 1
 				bit_counter--;
 				bcd_out <= shift_register[total_size_in_bits-1:input_size_in_bits]; // upper part
 			end else begin
-				if (bit_counter[0]==1) begin
+				if (bit_counter[0] == 0) begin // bit_counter is 18,16,14,12,10,8,6,4,2
 					bit_counter--;
 					shift_register[total_size_in_bits-1:1] <= shift_register[total_size_in_bits-2:0];
-					//shift_register[input_size_in_bits+7:input_size_in_bits] <= hex_in;
-				end else begin
+				end else begin // bit_counter is 17,15,13,11,9,7,5,3
 					bit_counter--;
-//					shift_register[0] <= 0;
-					for (offset = 0; offset < output_size_in_bits; offset = offset + 4)
+//					shift_register[0] <= 0; // not necessary because it ends before dealing with these
+					for (offset = 0; offset < output_size_in_bits; offset = offset + 4) begin
 						if (shift_register[input_size_in_bits+3+offset:input_size_in_bits+offset] >= 5) begin
 							shift_register[input_size_in_bits+3+offset:input_size_in_bits+offset] <= shift_register[input_size_in_bits+3+offset:input_size_in_bits+offset] + 3; // add 3 to the nybble if >= 5; input nybble can be at most 4'b0111 because of being shifted in one bit at a time, so the result can be 4'b1010 at most which doesn't overflow
 						end
+					end
 				end
 			end
 		end
@@ -48,7 +46,7 @@ module top(input CLK, input J3_3, output LED5, output LED4, output LED3, output 
 	reg [31:0] counter;
 	wire reset;
 	always @(posedge CLK) begin
-		if (counter[31:24]==0) begin
+		if (counter[31:16]==0) begin
 			reset <= 1;
 		end else begin
 			reset <= 0;
@@ -56,15 +54,18 @@ module top(input CLK, input J3_3, output LED5, output LED4, output LED3, output 
 		counter++;
 	end
 	hex2bcd h2binst ( .clock(CLK), .reset(reset), .hex_in(counter[31:24]), .bcd_out(bcd) );
-	assign LED5 = bcd[4];
-	assign LED4 = bcd[3];
-	assign LED3 = bcd[2];
-	assign LED2 = bcd[1];
-	assign LED1 = bcd[0];
-//	assign LED5 = counter[28];
-//	assign LED4 = counter[27];
-//	assign LED3 = counter[26];
-//	assign LED2 = counter[25];
-//	assign LED1 = counter[24];
+	if (1) begin
+		assign LED5 = bcd[4];
+		assign LED4 = bcd[3];
+		assign LED3 = bcd[2];
+		assign LED2 = bcd[1];
+		assign LED1 = bcd[0];
+	end else begin
+		assign LED5 = 0;
+		assign LED4 = bcd[7];
+		assign LED3 = bcd[6];
+		assign LED2 = bcd[5];
+		assign LED1 = bcd[4];
+	end
 endmodule // top
 
