d00021986f7b446b85163b37ad579156f5b66aaa
mzandrew <mza@phys.hawaii.edu>
2018-07-31 00:46:53 +0000

    put modules in library files in src/lib/

--- verilog/src/mza-test003.double-dabble.v
+++ verilog/src/mza-test003.double-dabble.v
@@ -1,45 +1,7 @@
 // written 2018-06-29 by mza
-// last updated 2018-06-29 by mza
+// last updated 2018-07-30 by mza
 
-// implemented after watching computerphile video https://www.youtube.com/watch?v=eXIfZ1yKFlA
-// each nybble in can be at most 15, which spans about 1.5 bcd nybbles, so 8 bits ("ff") -> 12 bits ("255")
-
-module hex2bcd(input clock, input reset, input [7:0] hex_in, output reg [11:0] bcd_out);
-	parameter input_size_in_bits = 8;
-	parameter output_size_in_bits = 12;
-	parameter total_size_in_bits = input_size_in_bits + output_size_in_bits;
-	reg [total_size_in_bits-1:0] shift_register; // size of input plus size of output
-	reg [7:0] bit_counter;
-	integer offset = 0;
-	always @(posedge clock) begin
-		if (reset) begin
-			bcd_out <= 0;
-			bit_counter <= 0;
-		end else begin
-			if (bit_counter == 0) begin // bit_counter is 0
-				bit_counter <= (input_size_in_bits << 1); // should be 16
-				shift_register[input_size_in_bits-1:0] <= hex_in; // lower part
-				shift_register[total_size_in_bits-1:input_size_in_bits] <= 0; // upper part
-			end else if (bit_counter == 1) begin // bit_counter is 1
-				bit_counter--;
-				bcd_out <= shift_register[total_size_in_bits-1:input_size_in_bits]; // upper part
-			end else begin
-				if (bit_counter[0] == 0) begin // bit_counter is 18,16,14,12,10,8,6,4,2
-					bit_counter--;
-					shift_register[total_size_in_bits-1:1] <= shift_register[total_size_in_bits-2:0];
-				end else begin // bit_counter is 17,15,13,11,9,7,5,3
-					bit_counter--;
-//					shift_register[0] <= 0; // not necessary because it ends before dealing with these
-					for (offset = 0; offset < output_size_in_bits; offset = offset + 4) begin
-						if (shift_register[input_size_in_bits+3+offset:input_size_in_bits+offset] >= 5) begin
-							shift_register[input_size_in_bits+3+offset:input_size_in_bits+offset] <= shift_register[input_size_in_bits+3+offset:input_size_in_bits+offset] + 3; // add 3 to the nybble if >= 5; input nybble can be at most 4'b0111 because of being shifted in one bit at a time, so the result can be 4'b1010 at most which doesn't overflow
-						end
-					end
-				end
-			end
-		end
-	end
-endmodule // bcd2hex
+`include "lib/hex2bcd.v"
 
 module top(input CLK, input J3_3, output LED5, output LED4, output LED3, output LED2, output LED1);
 	reg [11:0] bcd;
--- verilog/src/mza-test008.16-segment-driver.button-debounce-duration-counter.v
+++ verilog/src/mza-test008.16-segment-driver.button-debounce-duration-counter.v
@@ -2,49 +2,8 @@
 // based on mza-test004.16-segment-driver.v
 // last updated 2018-07-30 by mza
 
-module debounce(input clock, input polarity, input raw_button_input, output button_active);
-	localparam timeout = 120000; // 10ms
-	reg [$clog2(timeout)+1:0] counter;
-	reg old_status = 0;
-	reg new_status = 0;
-	always @(posedge clock) begin
-		if (counter==0) begin // idle, so time to check raw_button_input...
-			if (raw_button_input==polarity) begin
-				new_status <= 1; // raw_button_input is active
-				if (old_status==0) begin // old_status != new_status, so act, then implement delay before checking again
-					counter <= timeout;
-					button_active <= 1;
-				end
-			end else begin
-				new_status <= 0; // raw_button_input is inactive
-				if (old_status==1) begin // old_status != new_status, so implement delay before checking again
-					counter <= timeout;
-					button_active <= 0;
-				end
-			end
-		end else begin
-			counter--;
-			button_active <= new_status;
-		end
-		old_status <= new_status;
-	end
-endmodule // debounce
-
-module edge_to_pulse(input clock, input polarity, input raw_input, output processed_output);
-// .polarity(1) means use positive logic (detect rising edge)
-	reg state;
-	always @(posedge clock) begin
-		processed_output <= ! polarity;
-		if (raw_input==polarity) begin
-			if (state!=polarity) begin
-				processed_output <= polarity;
-				state <= polarity;
-			end
-		end else begin
-			state <= ! polarity;
-		end
-	end
-endmodule // edge_to_pulse
+`include "lib/debounce.v"
+`include "lib/edge_to_pulse.v"
 
 module top(input CLK, 
 input J3_10, 
