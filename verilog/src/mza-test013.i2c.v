4c2912b0108c37a89c541db43ce4ab20290fdf40
mzandrew <mza@phys.hawaii.edu>
2018-08-09 17:01:29 +0000

    gets humidity and temperature data out

--- verilog/src/mza-test013.i2c.v
+++ verilog/src/mza-test013.i2c.v
@@ -33,148 +33,256 @@ input sda_in
 	wire i2c_clock;
 	assign i2c_clock = counter[5];
 //	assign scl = i2c_clock;
-	reg [7:0] bit_counter;
+	reg [8:0] bit_counter;
 	reg ack;
 	reg error;
 	reg [7:0] data;
+	reg [13:0] humidity;
+	reg [13:0] temperature;
 	always @(posedge i2c_clock) begin
 		if (bit_counter>0) begin
 			case(bit_counter)
-				250 : begin
+				300 : begin
 					sda_dir <= 1;
 					scl <= 1;
 					sda_out <= 1;
 				end
 
 				// send start or repeated start
-				239 : sda_out <= 1;
-				238 : scl <= 1;
-				237 : sda_out <= 0; // start condition
-				236 : scl <= 0;
+				289 : sda_out <= 1;
+				288 : scl <= 1;
+				287 : sda_out <= 0; // start condition
+				286 : scl <= 0;
 
 				// send address word
-				228 : sda_out <= i2c_address[6]; // byte[7]
-				227 : scl <= 1;
-				226 : scl <= 0;
-				225 : sda_out <= i2c_address[5]; // byte[6]
-				224 : scl <= 1;
-				223 : scl <= 0;
-				222 : sda_out <= i2c_address[4]; // byte[5]
-				221 : scl <= 1;
-				220 : scl <= 0;
-				219 : sda_out <= i2c_address[3]; // byte[4]
-				218 : scl <= 1;
-				217 : scl <= 0;
-				216 : sda_out <= i2c_address[2]; // byte[3]
-				215 : scl <= 1;
-				214 : scl <= 0;
-				213 : sda_out <= i2c_address[1]; // byte[2]
-				212 : scl <= 1;
-				211 : scl <= 0;
-				210 : sda_out <= i2c_address[0]; // byte[1]
-				209 : scl <= 1;
-				208 : scl <= 0;
-				207 : sda_out <= 0; // byte[0] = 0; write
-				206 : scl <= 1;
-				205 : scl <= 0;
+				278 : sda_out <= i2c_address[6]; // byte[7]
+				277 : scl <= 1;
+				276 : scl <= 0;
+				275 : sda_out <= i2c_address[5]; // byte[6]
+				274 : scl <= 1;
+				273 : scl <= 0;
+				272 : sda_out <= i2c_address[4]; // byte[5]
+				271 : scl <= 1;
+				270 : scl <= 0;
+				269 : sda_out <= i2c_address[3]; // byte[4]
+				268 : scl <= 1;
+				267 : scl <= 0;
+				266 : sda_out <= i2c_address[2]; // byte[3]
+				265 : scl <= 1;
+				264 : scl <= 0;
+				263 : sda_out <= i2c_address[1]; // byte[2]
+				262 : scl <= 1;
+				261 : scl <= 0;
+				260 : sda_out <= i2c_address[0]; // byte[1]
+				259 : scl <= 1;
+				258 : scl <= 0;
+				257 : sda_out <= 0; // byte[0] = 0; write
+				256 : scl <= 1;
+				255 : scl <= 0;
 
 				// get ack
-				199 : sda_dir <= 0; // input
-				198 : scl <= 1;
-				197 : ack <= sda_in; // ack
-				196 : scl <= 0;
-				195 : sda_dir <= 1; // output
-				194 : if (ack==1) begin bit_counter <= 10; error <= 1; end
+				249 : sda_dir <= 0; // input
+				248 : scl <= 1;
+				247 : ack <= sda_in; // ack
+				246 : scl <= 0;
+				245 : sda_dir <= 1; // output
+				244 : if (ack==1) begin bit_counter <= 10; error <= 1; end
 
 				// send stop
-				190 : sda_out <= 1;
-				189 : sda_out <= 0;
-				188 : scl <= 1;
-				187 : sda_out <= 1;
+				230 : sda_out <= 1;
+				239 : sda_out <= 0;
+				238 : scl <= 1;
+				237 : sda_out <= 1;
 
 				// send start or repeated start
-				179 : sda_out <= 1;
-				178 : scl <= 1;
-				177 : sda_out <= 0; // start condition
-				176 : scl <= 0;
+				229 : sda_out <= 1;
+				228 : scl <= 1;
+				227 : sda_out <= 0; // start condition
+				226 : scl <= 0;
 
 				// send address word
-				169 : sda_out <= i2c_address[6]; // byte[7]
-				168 : scl <= 1;
-				167 : scl <= 0;
-				166 : sda_out <= i2c_address[5]; // byte[6]
-				165 : scl <= 1;
-				164 : scl <= 0;
-				163 : sda_out <= i2c_address[4]; // byte[5]
-				162 : scl <= 1;
-				161 : scl <= 0;
-				160 : sda_out <= i2c_address[3]; // byte[4]
-				159 : scl <= 1;
-				158 : scl <= 0;
-				157 : sda_out <= i2c_address[2]; // byte[3]
-				156 : scl <= 1;
-				155 : scl <= 0;
-				154 : sda_out <= i2c_address[1]; // byte[2]
-				153 : scl <= 1;
-				152 : scl <= 0;
-				151 : sda_out <= i2c_address[0]; // byte[1]
-				150 : scl <= 1;
-				149 : scl <= 0;
-				148 : sda_out <= 1; // byte[0] = 1; read
-				147 : scl <= 1;
-				146 : scl <= 0;
+				219 : sda_out <= i2c_address[6]; // byte[7]
+				218 : scl <= 1;
+				217 : scl <= 0;
+				216 : sda_out <= i2c_address[5]; // byte[6]
+				215 : scl <= 1;
+				214 : scl <= 0;
+				213 : sda_out <= i2c_address[4]; // byte[5]
+				212 : scl <= 1;
+				211 : scl <= 0;
+				210 : sda_out <= i2c_address[3]; // byte[4]
+				209 : scl <= 1;
+				208 : scl <= 0;
+				207 : sda_out <= i2c_address[2]; // byte[3]
+				206 : scl <= 1;
+				205 : scl <= 0;
+				204 : sda_out <= i2c_address[1]; // byte[2]
+				203 : scl <= 1;
+				202 : scl <= 0;
+				201 : sda_out <= i2c_address[0]; // byte[1]
+				200 : scl <= 1;
+				199 : scl <= 0;
+				198 : sda_out <= 1; // byte[0] = 1; read
+				197 : scl <= 1;
+				196 : scl <= 0;
 
 				// get ack
-				139 : sda_dir <= 0; // input
-				138 : scl <= 1;
-				137 : ack <= sda_in; // ack
-				136 : scl <= 0;
-				135 : sda_dir <= 1; // output
-				134 : if (ack==1) begin bit_counter <= 10; error <= 1; end
+				189 : sda_dir <= 0; // input
+				188 : scl <= 1;
+				187 : ack <= sda_in; // ack
+				186 : scl <= 0;
+				185 : sda_dir <= 1; // output
+				184 : if (ack==1) begin bit_counter <= 10; error <= 1; end
 
-				129 : sda_out <= 1;
-				128 : sda_out <= 0;
+				179 : sda_out <= 1;
+				178 : sda_out <= 0;
 
 				// get data word
-				119 : sda_dir <= 0; // input
-				119 : scl <= 1;
-				118 : data[7] <= sda_in;
-				117 : scl <= 0;
-				116 : scl <= 1;
-				115 : data[6] <= sda_in;
-				114 : scl <= 0;
-				113 : scl <= 1;
-				112 : data[5] <= sda_in;
-				111 : scl <= 0;
-				110 : scl <= 1;
-				109 : data[4] <= sda_in;
-				108 : scl <= 0;
-				107 : scl <= 1;
-				106 : data[3] <= sda_in;
-				105 : scl <= 0;
-				104 : scl <= 1;
-				103 : data[2] <= sda_in;
-				102 : scl <= 0;
-				101 : scl <= 1;
-				100 : data[1] <= sda_in;
-				099 : scl <= 0;
-				098 : scl <= 1;
-				097 : data[0] <= sda_in;
-				096 : scl <= 0;
+				169 : sda_dir <= 0; // input
+				168 : scl <= 1;
+				167 : data[7] <= sda_in;
+				166 : scl <= 0;
+				165 : scl <= 1;
+				164 : data[6] <= sda_in;
+				163 : scl <= 0;
+				162 : scl <= 1;
+				161 : data[5] <= sda_in;
+				160 : scl <= 0;
+				159 : scl <= 1;
+				158 : data[4] <= sda_in;
+				157 : scl <= 0;
+				156 : scl <= 1;
+				155 : data[3] <= sda_in;
+				154 : scl <= 0;
+				153 : scl <= 1;
+				152 : data[2] <= sda_in;
+				151 : scl <= 0;
+				150 : scl <= 1;
+				149 : data[1] <= sda_in;
+				148 : scl <= 0;
+				147 : scl <= 1;
+				146 : data[0] <= sda_in;
+				145 : scl <= 0;
 				// end of data word
+				144 : humidity[13:8] <= data[6:0];
 
 				// send ack
-				089 : sda_dir <= 1; // output
+				139 : sda_dir <= 1; // output
+				138 : sda_out <= 0; // ack
+				137 : scl <= 1;
+				136 : scl <= 0;
+
+				// get data word
+				129 : sda_dir <= 0; // input
+				128 : scl <= 1;
+				127 : data[7] <= sda_in;
+				126 : scl <= 0;
+				125 : scl <= 1;
+				124 : data[6] <= sda_in;
+				123 : scl <= 0;
+				122 : scl <= 1;
+				121 : data[5] <= sda_in;
+				120 : scl <= 0;
+				119 : scl <= 1;
+				118 : data[4] <= sda_in;
+				117 : scl <= 0;
+				116 : scl <= 1;
+				115 : data[3] <= sda_in;
+				114 : scl <= 0;
+				113 : scl <= 1;
+				112 : data[2] <= sda_in;
+				111 : scl <= 0;
+				110 : scl <= 1;
+				109 : data[1] <= sda_in;
+				108 : scl <= 0;
+				107 : scl <= 1;
+				106 : data[0] <= sda_in;
+				105 : scl <= 0;
+				// end of data word
+				104 : humidity[7:0] <= data;
+
+				// send ack
+				099 : sda_dir <= 1; // output
+				098 : sda_out <= 0; // ack
+				097 : scl <= 1;
+				096 : scl <= 0;
+
+				// get data word
+				089 : sda_dir <= 0; // input
 				088 : scl <= 1;
-				087 : sda_out <= 0; // ack
+				087 : data[7] <= sda_in;
 				086 : scl <= 0;
-				085 : sda_dir <= 0; // input
+				085 : scl <= 1;
+				084 : data[6] <= sda_in;
+				083 : scl <= 0;
+				082 : scl <= 1;
+				081 : data[5] <= sda_in;
+				080 : scl <= 0;
+				079 : scl <= 1;
+				078 : data[4] <= sda_in;
+				077 : scl <= 0;
+				076 : scl <= 1;
+				075 : data[3] <= sda_in;
+				074 : scl <= 0;
+				073 : scl <= 1;
+				072 : data[2] <= sda_in;
+				071 : scl <= 0;
+				070 : scl <= 1;
+				069 : data[1] <= sda_in;
+				068 : scl <= 0;
+				067 : scl <= 1;
+				066 : data[0] <= sda_in;
+				065 : scl <= 0;
+				// end of data word
+				064 : temperature[13:6] <= data;
+
+				// send ack
+				059 : sda_dir <= 1; // output
+				058 : sda_out <= 0; // ack
+				057 : scl <= 1;
+				056 : scl <= 0;
+
+				// get data word
+				049 : sda_dir <= 0; // input
+				048 : scl <= 1;
+				047 : data[7] <= sda_in;
+				046 : scl <= 0;
+				045 : scl <= 1;
+				044 : data[6] <= sda_in;
+				043 : scl <= 0;
+				042 : scl <= 1;
+				041 : data[5] <= sda_in;
+				040 : scl <= 0;
+				039 : scl <= 1;
+				038 : data[4] <= sda_in;
+				037 : scl <= 0;
+				036 : scl <= 1;
+				035 : data[3] <= sda_in;
+				034 : scl <= 0;
+				033 : scl <= 1;
+				032 : data[2] <= sda_in;
+				031 : scl <= 0;
+				030 : scl <= 1;
+				029 : data[1] <= sda_in;
+				028 : scl <= 0;
+				027 : scl <= 1;
+				026 : data[0] <= sda_in;
+				025 : scl <= 0;
+				// end of data word
+				024 : temperature[5:0] <= data[7:2];
+
+				// send ack
+				019 : sda_dir <= 1; // output
+				018 : sda_out <= 0; // ack
+				017 : scl <= 1;
+				016 : scl <= 0;
 
 				// send stop
-				009 : sda_out <= 1;
-				008 : sda_out <= 0;
-				007 : scl <= 1;
-				006 : sda_out <= 1;
+				009 : sda_dir <= 1; // output
+				008 : sda_out <= 1;
+				007 : sda_out <= 0;
+				006 : scl <= 1;
+				005 : sda_out <= 1;
 
 				001 : begin
 					sda_dir <= 1;
@@ -185,7 +293,7 @@ input sda_in
 			endcase
 			bit_counter--;
 		end else begin
-			bit_counter <= 250;
+			bit_counter <= 300;
 			error <= 0;
 		end
 	end
