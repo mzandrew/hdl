5975ccdef6534358db7a5d01bf37cedd38a6dc7f
mzandrew <mza@phys.hawaii.edu>
2018-09-18 14:04:23 -1000

    bugfix; enable LEDs for debug; including openocd config; outputs 250 MHz from LEMO connector

--- verilog/src/mza-test018.clock-in-to-toggle-out.v
+++ verilog/src/mza-test018.clock-in-to-toggle-out.v
@@ -1,20 +1,33 @@
 `timescale 1ns / 1ps
 // written 2018-09-17 by mza
-// last updated 2018-09-17 by mza
+// last updated 2018-09-18 by mza
 
 module mytop(
 	input clock_p,
 	input clock_n,
-	output ttl_trig_output
+	output ttl_trig_output,
+	output led0,
+	output led1,
+	output led2
 	//output lvds_trig_output_p,
 	//output lvds_trig_output_n
 );
 	reg trigout;
 	assign ttl_trig_output = trigout;
 	wire clock;
-	IBUFGDS clockbuf (.I(clock_p), .IB(clock_n), .O(clock));
+	reg [31:0] counter;
+	assign led0 = counter[28];
+	assign led1 = reset;
+	assign led2 = ~reset;
+	reg reset = 1;
+	IBUFGDS clockbuf (.I(clock_p), .IB(clock_n), .O(clock)); // 250 MHz
 	always @(posedge clock) begin
+		if (reset) begin
+			if (counter[10]) begin
+				reset = 0;
+			end
+		end
 		trigout <= ~ trigout;
+		counter <= counter + 1;
 	end
 endmodule
-
