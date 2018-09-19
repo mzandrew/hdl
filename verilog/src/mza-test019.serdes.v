d8f9f1b47b7a88ddfe3dab714cfefe9784b26733
mzandrew <mza@phys.hawaii.edu>
2018-09-19 07:08:00 -1000

    bypass serdes and just use oddr2 for now

--- verilog/src/mza-test019.serdes.v
+++ verilog/src/mza-test019.serdes.v
@@ -26,11 +26,12 @@ module mza_test019_serdes (
 	wire unbuffered_clock;
 	BUFIO2_2CLK buffy (.I(other_clock_p), .IB(other_clock_n), .DIVCLK(unbuffered_clock), .IOCLK(IOCLK0), .SERDESSTROBE(IOCE));
 	BUFG asdf (.I(unbuffered_clock), .O(clock));
-	OSERDES2 #(.DATA_RATE_OQ("SDR"), .DATA_RATE_OT("SDR"), .DATA_WIDTH(4),
-	           .OUTPUT_MODE("SINGLE_ENDED"), .SERDES_MODE("MASTER"))
-	         osirus
-	         (.OQ(ttl_trig_output), .CLK0(IOCLK0), .CLK1(1'b0), .CLKDIV(clock),
-	         .D1(1'b1), .D2(1'b0), .D3(1'b1), .D4(1'b1), .IOCE(IOCE), .OCE(1'b1), .RST(reset), .TCE(1'b1), .TRAIN(1'b0));
+//	OSERDES2 #(.DATA_RATE_OQ("SDR"), .DATA_RATE_OT("SDR"), .DATA_WIDTH(4),
+//	           .OUTPUT_MODE("SINGLE_ENDED"), .SERDES_MODE("MASTER"))
+//	         osirus
+//	         (.OQ(ttl_trig_output), .CLK0(IOCLK0), .CLK1(1'b0), .CLKDIV(clock),
+//	         .D1(counter[0]), .D2(counter[1]), .D3(counter[2]), .D4(counter[3]),
+//	         .IOCE(IOCE), .OCE(1'b1), .RST(reset), .TCE(1'b0), .TRAIN(1'b0));
 	always @(posedge clock) begin
 		if (reset) begin
 			if (counter[10]) begin
@@ -39,4 +40,6 @@ module mza_test019_serdes (
 		end
 		counter <= counter + 1;
 	end
+	ODDR2 ogre (.Q(ttl_trig_output), .C0(other_clock_p), .C1(other_clock_n), .D0(1'b0), .D1(1'b1), .S(1'b0), .R(reset), .CE(1'b1));
+//	assign ttl_trig_output = counter[0];
 endmodule
