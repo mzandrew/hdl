08a64c2c4ff068085a89cbdbd4eb944b7f7dbcbb
mzandrew <mza@phys.hawaii.edu>
2018-09-18 23:29:08 -1000

    bugfixes; misunderstood IO clocking paradigm(s)

--- verilog/src/mza-test019.serdes.v
+++ verilog/src/mza-test019.serdes.v
@@ -22,25 +22,28 @@ module mza_test019_serdes (
 	reg reset = 1;
 //	wire clock_inverted;
 //	assign clock_inverted = ~ clock;
-	IBUFDS clockbuf (.I(clock_p), .IB(clock_n), .O(clock)); // 250 MHz
+//	IBUFDS clockbuf (.I(clock_p), .IB(clock_n), .O(clock)); // 250 MHz
 //	wire another_clock;
 //	ODDR2 ogre (.Q(another_clock), .C0(clock), .C1(~clock), .D0(0), .D1(1), .S(0), .R(0));
-	wire tog;
-	FDCE fargo (.D(~tog), .CE(1'b1), .C(clock), .CLR(0), .Q(tog));
+//	wire tog;
+//	FDCE fargo (.D(~tog), .CE(1'b1), .C(clock), .CLR(0), .Q(tog));
 	wire other_clock_p;
 	wire other_clock_n;
-//	IBUFDS_DIFF_OUT wtf_xilinx (.I(clock_p), .IB(clock_n), .O(other_clock_p), .OB(other_clock_n));
-	wire DIVCLK;
-	wire CLKDIV;
+	IBUFDS_DIFF_OUT wtf_xilinx (.I(clock_p), .IB(clock_n), .O(other_clock_p), .OB(other_clock_n));
+//	wire DIVCLK;
+//	wire CLKDIV;
 	wire CLK0;
 	wire IOCE;
-	BUFIO2 #(.USE_DOUBLER("FALSE")) buffy (.I(tog), .DIVCLK(DIVCLK), .IOCLK(CLK0), .SERDESSTROBE(IOCE));
-//	BUFIO2_2CLK buffy (.I(other_clock_p), .IB(other_clock_n), .DIVCLK(clock), .IOCLK(CLK0), .SERDESSTROBE(IOCE));
-	BUFG asdf (.I(DIVCLK), .O(CLKDIV));
+	wire unbuffered_clock;
+//	BUFIO2 #(.USE_DOUBLER("FALSE")) buffy (.I(tog), .DIVCLK(DIVCLK), .IOCLK(CLK0), .SERDESSTROBE(IOCE));
+	BUFIO2_2CLK buffy (.I(other_clock_p), .IB(other_clock_n), .DIVCLK(unbuffered_clock), .IOCLK(CLK0), .SERDESSTROBE(IOCE));
+//	BUFIO2_2CLK buffy (.I(clock_p), .IB(clock_n), .DIVCLK(clock), .IOCLK(CLK0), .SERDESSTROBE(IOCE));
+//	BUFG asdf (.I(DIVCLK), .O(CLKDIV));
+	BUFG asdf (.I(unbuffered_clock), .O(clock));
 	OSERDES2 #(.DATA_RATE_OQ("SDR"), .DATA_RATE_OT("SDR"), .DATA_WIDTH(4),
 	           .OUTPUT_MODE("SINGLE_ENDED"), .SERDES_MODE("MASTER"))
 	         osirus
-	         (.OQ(ttl_trig_output), .CLK0(CLK0), .CLK1(0), .CLKDIV(CLKDIV),
+	         (.OQ(ttl_trig_output), .CLK0(CLK0), .CLK1(0), .CLKDIV(clock),
 	         .D1(1), .D2(0), .D3(1), .D4(1), .IOCE(IOCE), .OCE(1), .RST(reset), .TCE(1), .TRAIN(0));
 	always @(posedge clock) begin
 		if (reset) begin
