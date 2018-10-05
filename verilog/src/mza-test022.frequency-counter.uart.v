00a9429ff8217cbef4a241863c755da734a81a0c
mzandrew <mza@phys.hawaii.edu>
2018-10-05 19:26:59 +0000

    code cleanup

--- verilog/src/mza-test022.frequency-counter.uart.v
+++ verilog/src/mza-test022.frequency-counter.uart.v
@@ -1,6 +1,6 @@
 // written 2018-09-27 by mza
 // based on mza-test014.duration-timer.uart.v
-// last updated 2018-10-04 by mza
+// last updated 2018-10-05 by mza
 
 `include "lib/hex2bcd.v"
 `include "lib/segmented_display_driver.v"
@@ -46,7 +46,7 @@ module mytop (
 	assign J1[6] = 0;
 	assign J1[7] = 0;
 	assign trigger_active = reference_clock_counter[log2_of_divide_ratio];
-//	assign signal_output = trigger_active;
+	assign signal_output = trigger_active;
 	assign J1[0] = signal_output; // trigger_out on PCB
 	always @(posedge reference_clock) begin
 		reference_clock_counter++;
@@ -64,15 +64,11 @@ module mytop (
 		trigger_stream <= { trigger_stream[1:0], trigger_active }; // 110, 100, 000 or 001, 011, 111
 	end
 	// for a pair of 4-digit 7-segment(+dp) TCMG1050M displays on a "icestick frequency counter revA" board
-	//wire [6:0] segment;
-	//assign { J1[4], J1[1], J3[4], J3[5], J1[2], J1[5], J1[3] } = segment; // segments g,f,e,d,c,b,a ; dp=J3[2]
 	wire [7:0] segment;
 	assign { J1[4], J1[1], J3[4], J3[5], J1[2], J1[5], J1[3], J3[2] } = segment; // segments g,f,e,d,c,b,a,dp
-	//assign J3[2] = 1;
 	wire [7:0] anode;
 	assign { J2[7], J2[4], J2[5], J2[6], J3[6], J3[7], J3[3], J3[1] } = anode; // anodes 7,6,5,4,3,2,1,0
-	//segmented_display_driver #(.number_of_segments(7), .number_of_nybbles(8)) my_segmented_display_driver (.clock(clock), .data(buffered_bcd2[31:0]), .cathode(segment), .anode(anode), .sync_a(signal_output), .sync_c(), .dp(0));
-	segmented_display_driver #(.number_of_segments(8), .number_of_nybbles(8)) my_segmented_display_driver (.clock(clock), .data(buffered_bcd2[31:0]), .cathode(segment), .anode(anode), .sync_a(signal_output), .sync_c(), .dp(8'b00100000));
+	segmented_display_driver #(.number_of_segments(8), .number_of_nybbles(8)) my_segmented_display_driver (.clock(clock), .data(buffered_bcd2[31:0]), .cathode(segment), .anode(anode), .sync_a(), .sync_c(), .dp(8'b00100000)); // need to change dp based on N
 	assign LED[5] = 0;
 	assign LED[4] = signal_output;
 	assign LED[3] = trigger_stream[2];
@@ -89,9 +85,6 @@ module mytop (
 	wire uart_resetb;
 	reg reset = 1;
 	assign uart_resetb = ~reset;
-//	localparam log2_of_function_generator_period = uart_line_pickoff;
-//	localparam function_generator_start = 0;
-//	reg [9:0] pulse_duration;
 //	reg [msb_of_counters:0] previous_number_of_pulses = 0;
 //	reg [msb_of_counters:0] number_of_pulses = 0;
 	always @(posedge clock) begin
@@ -100,37 +93,16 @@ module mytop (
 //			uart_line_counter <= 0;
 //			uart_character_counter <= length_of_line - 1;
 //			uart_transfers_are_allowed <= 0;
-//			signal_output <= 0;
 			if (counter[10]==1) begin
 				reset <= 0;
 			end
-//		end else begin
-//			//if (counter[31:0]>1500 & counter[31:0]<2000) begin
-//			if (counter[log2_of_function_generator_period:0]>function_generator_start) begin
-//				if (counter[log2_of_function_generator_period:0]<function_generator_start+pulse_duration) begin
-//					signal_output <= 1;
-//				end else begin
-//					signal_output <= 0;
-//				end
-//			end else begin
-//				signal_output <= 0;
-//			end
 		end
 		if (counter[slow_clock_pickoff:0]==0) begin
-//			buffered_bcd1 <= bcd1;
 			buffered_bcd2 <= bcd2;
 		end else if (counter[slow_clock_pickoff:0]==1) begin
-//			accumulator = previous_trigger_duration * frequency_of_reference_clock_in_N_Hz;
 			result = { 0, previous_accumulator[msb_of_accumulator:log2_of_divide_ratio] };
 		end else if (counter[slow_clock_pickoff:0]==2) begin
-//		end else if (counter[slow_clock_pickoff:0]==3) begin
-			//value1 <= uart_line_counter;
-//			value1 <= reference_clock_counter[23:0];
-//			value2 <= previous_trigger_duration; // TDC mode
-//			value2 <= accumulator[msb_of_accumulator:log2_of_divide_ratio]; // frequency counter mode
-//			value2 <= accumulator[23:0]; // frequency counter mode
 			value2 <= result; // frequency counter mode
-//			value1 <= number_of_pulses; // scaler mode
 		end
 //		if (counter[uart_line_pickoff:0]==0) begin // less frequent
 //			if (previous_number_of_pulses!=number_of_pulses) begin
