22ba9d16b9b56b744cc32b402f8d5092127e68fe
mzandrew <mza@phys.hawaii.edu>
2018-11-16 11:23:40 -1000

    split oserdes_pll module to be one pll_adv and two bufplls; other minor changes to get it to work on the board

--- verilog/src/lib/serdes_pll.v
+++ verilog/src/lib/serdes_pll.v
@@ -1,20 +1,28 @@
 `timescale 1ns / 1ps
 // written 2018-09-17 by mza
-// last updated 2018-10-24 by mza
+// last updated 2018-11-16 by mza
 
-// 156.25 / 8.0 * 61.875 / 2.375 = 508.840461
-// 156.25 / 5 * 32 = 1000
-module oserdes_pll #(parameter WIDTH=8, parameter CLKIN_PERIOD=6.4, parameter PLLD=5, parameter PLLX=32) (input clock_in, input reset, output fabric_clock_out, output serializer_clock_out, output serializer_strobe_output, output locked);
+// 156.25 / 8.0 * 61.875 / 2.375 = 508.840461 for scrod revA3 on-board oscillator
+// 156.25 / 5 * 32 = 1000 for scrod revA3 on-board oscillator
+// 50.0 / 2 * 40 = 1000 for althea on-board oscillator
+module oserdes_pll #(parameter WIDTH=8, parameter CLKIN_PERIOD=6.4, parameter PLLD=5, parameter PLLX=32) (
+	input clock_in, input reset, output fabric_clock_out,
+	output serializer_clock_out_1, output serializer_strobe_out_1, output locked_1,
+	output serializer_clock_out_2, output serializer_strobe_out_2, output locked_2
+);
 	// from clock_generator_pll_s8_diff.v from XAPP1064 example code
 //	localparam integer PLLD = 5; // 1 to 52 on a spartan6
 //	localparam integer PLLX = 32; // 1 to 64 on a spartan6
 	// frequency of VCO after div and mult must be in range [400,1080] MHz
+	wire pllout_xn_1; // pll generated xn clock
+	wire pllout_xn_2; // pll generated xn clock
 	wire pllout_x1; // pll generated x1 clock
-	wire pllout_xn; // pll generated xn clock
-	wire dummy; // feedback net
+	wire fb; // feedback net
 	wire pll_is_locked; // Locked output from PLL
-	wire buffered_pll_is_locked_and_strobe_is_aligned;
-	assign locked = pll_is_locked & buffered_pll_is_locked_and_strobe_is_aligned;
+	wire buffered_pll_is_locked_and_strobe_is_aligned_1;
+	wire buffered_pll_is_locked_and_strobe_is_aligned_2;
+	assign locked_1 = pll_is_locked & buffered_pll_is_locked_and_strobe_is_aligned_1;
+	assign locked_2 = pll_is_locked & buffered_pll_is_locked_and_strobe_is_aligned_2;
 	PLL_ADV #(
 		.SIM_DEVICE("SPARTAN6"),
 		.BANDWIDTH("OPTIMIZED"), // "high", "low" or "optimized"
@@ -24,33 +32,33 @@ module oserdes_pll #(parameter WIDTH=8, parameter CLKIN_PERIOD=6.4, parameter PL
 		.DIVCLK_DIVIDE(PLLD), // division factor for all clocks (1 to 52)
 		.CLKFBOUT_MULT(PLLX), // multiplication factor for all output clocks
 		.CLKOUT0_DIVIDE(1), // division factor for clkout0 (1 to 128)
-		.CLKOUT0_DUTY_CYCLE(0.5), // duty cycle for clkout0 (0.01 to 0.99)
-		.CLKOUT0_PHASE(0.0), // phase shift (degrees) for clkout0 (0.0 to 360.0)
 		.CLKOUT1_DIVIDE(1), // division factor for clkout1 (1 to 128)
-		.CLKOUT1_DUTY_CYCLE(0.5), // duty cycle for clkout1 (0.01 to 0.99)
-		.CLKOUT1_PHASE(0.0), // phase shift (degrees) for clkout1 (0.0 to 360.0)
 		.CLKOUT2_DIVIDE(WIDTH), // division factor for clkout2 (1 to 128)
-		.CLKOUT2_DUTY_CYCLE(0.5), // duty cycle for clkout2 (0.01 to 0.99)
-		.CLKOUT2_PHASE(0.0), // phase shift (degrees) for clkout2 (0.0 to 360.0)
 		.CLKOUT3_DIVIDE(8), // division factor for clkout3 (1 to 128)
-		.CLKOUT3_DUTY_CYCLE(0.5), // duty cycle for clkout3 (0.01 to 0.99)
-		.CLKOUT3_PHASE(0.0), // phase shift (degrees) for clkout3 (0.0 to 360.0)
 		.CLKOUT4_DIVIDE(8), // division factor for clkout4 (1 to 128)
-		.CLKOUT4_DUTY_CYCLE(0.5), // duty cycle for clkout4 (0.01 to 0.99)
-		.CLKOUT4_PHASE(0.0), // phase shift (degrees) for clkout4 (0.0 to 360.0)
 		.CLKOUT5_DIVIDE(8), // division factor for clkout5 (1 to 128)
-		.CLKOUT5_DUTY_CYCLE(0.5), // duty cycle for clkout5 (0.01 to 0.99)
+		.CLKOUT0_PHASE(0.0), // phase shift (degrees) for clkout0 (0.0 to 360.0)
+		.CLKOUT1_PHASE(0.0), // phase shift (degrees) for clkout1 (0.0 to 360.0)
+		.CLKOUT2_PHASE(0.0), // phase shift (degrees) for clkout2 (0.0 to 360.0)
+		.CLKOUT3_PHASE(0.0), // phase shift (degrees) for clkout3 (0.0 to 360.0)
+		.CLKOUT4_PHASE(0.0), // phase shift (degrees) for clkout4 (0.0 to 360.0)
 		.CLKOUT5_PHASE(0.0), // phase shift (degrees) for clkout5 (0.0 to 360.0)
+		.CLKOUT0_DUTY_CYCLE(0.5), // duty cycle for clkout0 (0.01 to 0.99)
+		.CLKOUT1_DUTY_CYCLE(0.5), // duty cycle for clkout1 (0.01 to 0.99)
+		.CLKOUT2_DUTY_CYCLE(0.5), // duty cycle for clkout2 (0.01 to 0.99)
+		.CLKOUT3_DUTY_CYCLE(0.5), // duty cycle for clkout3 (0.01 to 0.99)
+		.CLKOUT4_DUTY_CYCLE(0.5), // duty cycle for clkout4 (0.01 to 0.99)
+		.CLKOUT5_DUTY_CYCLE(0.5), // duty cycle for clkout5 (0.01 to 0.99)
 		.COMPENSATION("INTERNAL"), // "SYSTEM_SYNCHRONOUS", "SOURCE_SYNCHRONOUS", "INTERNAL", "EXTERNAL", "DCM2PLL", "PLL2DCM"
 		.REF_JITTER(0.100) // input reference jitter (0.000 to 0.999 ui%)
 		) tx_pll_adv_inst (
 		.RST(reset), // asynchronous pll reset
 		.LOCKED(pll_is_locked), // active high pll lock signal
-		.CLKFBIN(dummy), // clock feedback input
-		.CLKFBOUT(dummy), // general output feedback signal
+		.CLKFBIN(fb), // clock feedback input
+		.CLKFBOUT(fb), // general output feedback signal
 		.CLKIN1(clock_in), // primary clock input
-		.CLKOUT0(pllout_xn), // *n clock for transmitter
-		.CLKOUT1(), //
+		.CLKOUT0(pllout_xn_1), // *n clock for transmitter
+		.CLKOUT1(pllout_xn_2), //
 		.CLKOUT2(pllout_x1), // *1 clock for BUFG
 		.CLKOUT3(), // one of six general clock output signals
 		.CLKOUT4(), // one of six general clock output signals
@@ -73,16 +81,28 @@ module oserdes_pll #(parameter WIDTH=8, parameter CLKIN_PERIOD=6.4, parameter PL
 		.DWE(1'b0), // dynamic reconfig write enable input
 		.REL(1'b0) // used to force the state of the PFD outputs (test only)
 		);
-	BUFG bufg_tx (.I(pllout_x1), .O(fabric_clock_out));
+	wire fabric_clock;
+	BUFG bufg_tx (.I(pllout_x1), .O(fabric_clock));
+	assign fabric_clock_out = fabric_clock;
 	BUFPLL #(
-		.DIVIDE(WIDTH) // PLLIN0 divide-by value to produce SERDESSTROBE (1 to 8); default 1
-		) tx_bufpll_inst (
-		.PLLIN(pllout_xn), // PLL Clock input
-		.GCLK(fabric_clock_out), // Global Clock input
+		.DIVIDE(WIDTH) // PLLIN divide-by value to produce SERDESSTROBE (1 to 8); default 1
+		) tx_bufpll_inst_1 (
+		.PLLIN(pllout_xn_1), // PLL Clock input
+		.GCLK(fabric_clock), // Global Clock input
 		.LOCKED(pll_is_locked), // Clock0 locked input
-		.IOCLK(serializer_clock_out), // Output PLL Clock
-		.LOCK(buffered_pll_is_locked_and_strobe_is_aligned), // BUFPLL Clock and strobe locked
-		.SERDESSTROBE(serializer_strobe_output) // Output SERDES strobe
+		.IOCLK(serializer_clock_out_1), // Output PLL Clock
+		.LOCK(buffered_pll_is_locked_and_strobe_is_aligned_1), // BUFPLL Clock and strobe locked
+		.SERDESSTROBE(serializer_strobe_out_1) // Output SERDES strobe
+		);
+	BUFPLL #(
+		.DIVIDE(WIDTH) // PLLIN divide-by value to produce SERDESSTROBE (1 to 8); default 1
+		) tx_bufpll_inst_2 (
+		.PLLIN(pllout_xn_2), // PLL Clock input
+		.GCLK(fabric_clock), // Global Clock input
+		.LOCKED(pll_is_locked), // Clock0 locked input
+		.IOCLK(serializer_clock_out_2), // Output PLL Clock
+		.LOCK(buffered_pll_is_locked_and_strobe_is_aligned_2), // BUFPLL Clock and strobe locked
+		.SERDESSTROBE(serializer_strobe_out_2) // Output SERDES strobe
 		);
 endmodule
 
--- verilog/src/mza-test024.serdes-pll-differential.althea.v
+++ verilog/src/mza-test024.serdes-pll-differential.althea.v
@@ -1,11 +1,12 @@
 `timescale 1ns / 1ps
 // written 2018-09-17 by mza
-// last updated 2018-10-25 by mza
+// last updated 2018-11-16 by mza
 
 module mza_test024_serdes_pll_differential_althea (
 	input clock_p,
 	input clock_n,
 	input self_triggered_mode_switch,
+	output lemo_output,
 	input lvds_trig_input_p,
 	input lvds_trig_input_n,
 	output lvds_trig_output_1_p,
@@ -27,14 +28,18 @@ module mza_test024_serdes_pll_differential_althea (
 	wire clock; // 125 MHz
 	reg [31:0] counter = 0;
 	reg sync;
-	wire other_clock;
-	IBUFGDS coolcool (.I(clock_p), .IB(clock_n), .O(other_clock)); // 156.25 MHz
-	wire lvds_trig_output_1;
-	OBUFDS catcat1 (.I(lvds_trig_output_1), .O(lvds_trig_output_1_p), .OB(lvds_trig_output_1_n));
-	wire lvds_trig_output_2;
-	OBUFDS catcat2 (.I(lvds_trig_output_2), .O(lvds_trig_output_2_p), .OB(lvds_trig_output_2_n));
-	wire IOCLK0;
-	wire IOCE;
+	wire other_clock; // 50.0 MHz
+	IBUFGDS coolcool (.I(clock_p), .IB(clock_n), .O(other_clock)); // 50.0 MHz
+	wire lvds_trig_output_R;
+//	OBUFDS catcat1 (.I(lvds_trig_output_R), .O(lvds_trig_output_1_p), .OB(lvds_trig_output_1_n));
+	wire lvds_trig_output_T;
+	OBUFDS catcat2 (.I(lvds_trig_output_T), .O(lvds_trig_output_2_p), .OB(lvds_trig_output_2_n));
+//	assign lemo_output = sync;
+	assign lemo_output = lvds_trig_output_R;
+	wire ioclk_T; // 1000 MHz
+	wire ioclk_R; // 1000 MHz
+	wire ioce_T;
+	wire ioce_R;
 	// with some help from https://vjordan.info/log/fpga/high-speed-serial-bus-generation-using-spartan-6.html and/or XAPP1064 source code
 	wire cascade_do1;
 	wire cascade_to1;
@@ -48,46 +53,46 @@ module mza_test024_serdes_pll_differential_althea (
 	localparam pickoff = 24;
 	wire [7:0] led_byte;
 	assign { led_7, led_6, led_5, led_4, led_3, led_2, led_1, led_0 } = led_byte;
-	assign led_byte = word;
+	assign led_byte = ~ word;
 	// want MSB of word to come out first
 	OSERDES2 #(.DATA_RATE_OQ("SDR"), .DATA_RATE_OT("SDR"), .DATA_WIDTH(WIDTH),
 	           .OUTPUT_MODE("SINGLE_ENDED"), .SERDES_MODE("MASTER"))
-	         osirus_master_1
-	         (.OQ(lvds_trig_output_1), .TQ(), .CLK0(IOCLK0), .CLK1(1'b0), .CLKDIV(clock),
+	         osirus_master_T
+	         (.OQ(lvds_trig_output_T), .TQ(), .CLK0(ioclk_T), .CLK1(1'b0), .CLKDIV(clock),
 	         .D1(word[3]), .D2(word[2]), .D3(word[1]), .D4(word[0]),
-	         .IOCE(IOCE), .OCE(1'b1), .RST(reset1), .TRAIN(1'b0),
+	         .IOCE(ioce_T), .OCE(1'b1), .RST(reset1), .TRAIN(1'b0),
 	         .SHIFTIN1(1'b1), .SHIFTIN2(1'b1), .SHIFTIN3(cascade_do1), .SHIFTIN4(cascade_to1), 
 	         .SHIFTOUT1(cascade_di1), .SHIFTOUT2(cascade_ti1), .SHIFTOUT3(), .SHIFTOUT4(), 
 	         .TCE(1'b1), .T1(1'b0), .T2(1'b0), .T3(1'b0), .T4(1'b0));
 	OSERDES2 #(.DATA_RATE_OQ("SDR"), .DATA_RATE_OT("SDR"), .DATA_WIDTH(WIDTH),
 	           .OUTPUT_MODE("SINGLE_ENDED"), .SERDES_MODE("SLAVE"))
-	         osirus_slave_1
-	         (.OQ(), .TQ(), .CLK0(IOCLK0), .CLK1(1'b0), .CLKDIV(clock),
+	         osirus_slave_T
+	         (.OQ(), .TQ(), .CLK0(ioclk_T), .CLK1(1'b0), .CLKDIV(clock),
 	         .D1(word[7]), .D2(word[6]), .D3(word[5]), .D4(word[4]),
-	         .IOCE(IOCE), .OCE(1'b1), .RST(reset1), .TRAIN(1'b0),
+	         .IOCE(ioce_T), .OCE(1'b1), .RST(reset1), .TRAIN(1'b0),
 	         .SHIFTIN1(cascade_di1), .SHIFTIN2(cascade_ti1), .SHIFTIN3(1'b1), .SHIFTIN4(1'b1),
 	         .SHIFTOUT1(), .SHIFTOUT2(), .SHIFTOUT3(cascade_do1), .SHIFTOUT4(cascade_to1),
 	         .TCE(1'b1), .T1(1'b0), .T2(1'b0), .T3(1'b0), .T4(1'b0));
 	OSERDES2 #(.DATA_RATE_OQ("SDR"), .DATA_RATE_OT("SDR"), .DATA_WIDTH(WIDTH),
 	           .OUTPUT_MODE("SINGLE_ENDED"), .SERDES_MODE("MASTER"))
-	         osirus_master_2
-	         (.OQ(lvds_trig_output_2), .TQ(), .CLK0(IOCLK0), .CLK1(1'b0), .CLKDIV(clock),
+	         osirus_master_R
+	         (.OQ(lvds_trig_output_R), .TQ(), .CLK0(ioclk_R), .CLK1(1'b0), .CLKDIV(clock),
 	         .D1(word[3]), .D2(word[2]), .D3(word[1]), .D4(word[0]),
-	         .IOCE(IOCE), .OCE(1'b1), .RST(reset1), .TRAIN(1'b0),
+	         .IOCE(ioce_R), .OCE(1'b1), .RST(reset1), .TRAIN(1'b0),
 	         .SHIFTIN1(1'b1), .SHIFTIN2(1'b1), .SHIFTIN3(cascade_do2), .SHIFTIN4(cascade_to2), 
 	         .SHIFTOUT1(cascade_di2), .SHIFTOUT2(cascade_ti2), .SHIFTOUT3(), .SHIFTOUT4(), 
 	         .TCE(1'b1), .T1(1'b0), .T2(1'b0), .T3(1'b0), .T4(1'b0));
 	OSERDES2 #(.DATA_RATE_OQ("SDR"), .DATA_RATE_OT("SDR"), .DATA_WIDTH(WIDTH),
 	           .OUTPUT_MODE("SINGLE_ENDED"), .SERDES_MODE("SLAVE"))
-	         osirus_slave_2
-	         (.OQ(), .TQ(), .CLK0(IOCLK0), .CLK1(1'b0), .CLKDIV(clock),
+	         osirus_slave_R
+	         (.OQ(), .TQ(), .CLK0(ioclk_R), .CLK1(1'b0), .CLKDIV(clock),
 	         .D1(word[7]), .D2(word[6]), .D3(word[5]), .D4(word[4]),
-	         .IOCE(IOCE), .OCE(1'b1), .RST(reset1), .TRAIN(1'b0),
+	         .IOCE(ioce_R), .OCE(1'b1), .RST(reset1), .TRAIN(1'b0),
 	         .SHIFTIN1(cascade_di2), .SHIFTIN2(cascade_ti2), .SHIFTIN3(1'b1), .SHIFTIN4(1'b1),
 	         .SHIFTOUT1(), .SHIFTOUT2(), .SHIFTOUT3(cascade_do2), .SHIFTOUT4(cascade_to2),
 	         .TCE(1'b1), .T1(1'b0), .T2(1'b0), .T3(1'b0), .T4(1'b0));
 	reg [12:0] reset1_counter = 0;
-	always @(posedge other_clock) begin
+	always @(posedge other_clock) begin // 50.0 MHz
 		if (reset1) begin
 			if (reset1_counter[10]) begin
 				reset1 <= 0;
@@ -99,11 +104,11 @@ module mza_test024_serdes_pll_differential_althea (
 	IBUFDS angel (.I(lvds_trig_input_p), .IB(lvds_trig_input_n), .O(trigger_input));
 	reg [1:0] token;
 	reg [2:0] trigger_stream;
-	localparam first  = 8'b11110000;
-	localparam second = 8'b10000001;
-	localparam third  = 8'b10001000;
-	localparam forth  = 8'b10101010;
-	always @(posedge clock) begin
+	localparam first  = ~ 8'b11110000;
+	localparam second = ~ 8'b10000001;
+	localparam third  = ~ 8'b10001000;
+	localparam forth  = ~ 8'b10101010;
+	always @(posedge clock) begin // 125.0 MHz
 		if (reset2) begin
 			token <= 2'b00;
 			trigger_stream <= 0;
@@ -111,7 +116,7 @@ module mza_test024_serdes_pll_differential_althea (
 				reset2 <= 0;
 			end
 		end
-		word <= 8'b00000000;
+		word <= ~ 8'b00000000;
 		if (self_triggered_mode_switch) begin
 			if (counter[pickoff:0]==0) begin
 				         if (counter[pickoff+2:pickoff+1]==2'b00) begin
@@ -147,6 +152,9 @@ module mza_test024_serdes_pll_differential_althea (
 		trigger_stream <= { trigger_stream[1:0], trigger_input };
 		counter <= counter + 1;
 	end
-//	oserdes_pll #(.WIDTH(WIDTH), .CLKIN_PERIOD(6.4), .PLLD(5), .PLLX(32)) difficult_pll (.reset(reset1), .clock_in(other_clock), .fabric_clock_out(clock), .serializer_clock_out(IOCLK0), .serializer_strobe_output(IOCE), .locked());
-	oserdes_pll #(.WIDTH(WIDTH), .CLKIN_PERIOD(20), .PLLD(2), .PLLX(40)) difficult_pll (.reset(reset1), .clock_in(other_clock), .fabric_clock_out(clock), .serializer_clock_out(IOCLK0), .serializer_strobe_output(IOCE), .locked());
+	oserdes_pll #(.WIDTH(WIDTH), .CLKIN_PERIOD(20.0), .PLLD(2), .PLLX(40)) difficult_pll_TR (
+		.reset(reset1), .clock_in(other_clock), .fabric_clock_out(clock), 
+		.serializer_clock_out_1(ioclk_T), .serializer_strobe_out_1(ioce_T), .locked_1(),
+		.serializer_clock_out_2(ioclk_R), .serializer_strobe_out_2(ioce_R), .locked_2()
+	);
 endmodule
