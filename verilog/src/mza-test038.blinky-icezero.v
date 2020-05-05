69504b4efa643b2fd45acf91c94ac8c3c8face83
mza <mza@phys.hawaii.edu>
2020-05-04 17:28:58 -1000

    well the button debounce module works, but it uses kind of a lot of flip-flops for what it does

--- verilog/src/mza-test038.blinky-icezero.v
+++ verilog/src/mza-test038.blinky-icezero.v
@@ -1,10 +1,12 @@
 // lifted from https://github.com/thekroko/icezero-blinky.git
-// last updated 2020-05-03 by mza
+// last updated 2020-05-04 by mza
 
 //`include "prescaler.v"
+`include "lib/synchronizer.v"
 
 module top (
 	input clk_100mhz,
+	input btn,
 	output led1,
 	output led2,
 	output led3
@@ -33,11 +35,22 @@ module top (
 	);
 `endif
 	reg [23:0] counter = 0;
-	always @(posedge clk_16mhz) begin
-		counter <= counter + 1'b1;
+	if (1) begin
+		wire button_clock;
+		button_debounce #(.DEBOUNCE_CLOCK_PERIODS(1678)) bd (.clock(clk_16mhz), .button_raw(btn), .button_just_went_active(button_clock));
+		always @(posedge button_clock) begin
+			counter <= counter + 1'b1;
+		end
+		assign led1 = counter[2];
+		assign led2 = counter[1];
+		assign led3 = counter[0];
+	end else begin
+		always @(posedge clk_16mhz) begin
+			counter <= counter + 1'b1;
+		end
+		assign led1 = counter[23];
+		assign led2 = counter[22];
+		assign led3 = counter[21];
 	end
-	assign led1 = counter[23];
-	assign led2 = counter[22];
-	assign led3 = counter[21];
 endmodule
 
