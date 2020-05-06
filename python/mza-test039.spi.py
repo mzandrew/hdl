011db814d1944c5f89a2f76ce8765137e043fcd7
mza <mza@phys.hawaii.edu>
2020-05-06 13:10:12 -1000

    show BER

--- python/mza-test039.spi.py
+++ python/mza-test039.spi.py
@@ -6,6 +6,7 @@
 import time
 import random
 import sys
+import math
 
 # from https://github.com/doceme/py-spidev
 import spidev
@@ -13,26 +14,73 @@ spi = spidev.SpiDev()
 bus = 0
 device = 0
 spi.open(bus, device)
-spi.max_speed_hz = int(20e6) # 20e6 works; 25e6 fails sometimes
+spi.max_speed_hz = int(24e6) # 24e6 works without errors; 25e6 fails often (BER=500e-3)
 spi.mode = 0b01
 
 def hex(number, width=1):
 	return "%0*x" % (width, number)
 
-byte_list = [ 0x00, 0x01, 0x03, 0x06, 0x04 ]
+# from https://stackoverflow.com/a/19270863/5728815
+#def eng(x, format='%s', si=False):
+def eng(x, format='%.1f', si=False):
+	'''
+	Returns float/int value <x> formatted in a simplified engineering format -
+	using an exponent that is a multiple of 3.
+	format: printf-style string used to format the value before the exponent.
+	si: if true, use SI suffix for exponent, e.g. k instead of e3, n instead of
+	e-9 etc.
+	E.g. with format='%.2f':
+	    1.23e-08 => 12.30e-9
+	         123 => 123.00
+	      1230.0 => 1.23e3
+	  -1230000.0 => -1.23e6
+	and with si=True:
+	      1230.0 => 1.23k
+	  -1230000.0 => -1.23M
+	'''
+	sign = ''
+	if x < 0.0:
+		x = -x
+		sign = '-'
+	exp = int(math.floor(math.log10(x)))
+	exp3 = exp - ( exp % 3)
+	x3 = x / ( 10 ** exp3)
+	if si and exp3 >= -24 and exp3 <= 24 and exp3 != 0:
+		exp3_text = 'yzafpnum kMGTPEZY'[ ( exp3 - (-24)) / 3]
+	elif exp3 == 0:
+		exp3_text = ''
+	else:
+		exp3_text = 'e%s' % exp3
+	return ( '%s'+format+'%s') % ( sign, x3, exp3_text)
+
+size = 100000
+
+#data_list = [ 0x00, 0x01, 0x03, 0x06, 0x04 ]
+data_list = []
+for i in range(size):
+	data_list.append(random.randint(0,255))
+
+total_transfers = 0
+total_errors = 0
 
 i = 0
 previous_value_written = 0
 while True:
-	for value_written in byte_list:
+	for value_written in data_list:
 		value_read, = spi.xfer([value_written])
+		total_transfers += 1
 		if 0!=i:
 			#print hex(previous_value_written, 1) + " " + hex(value_read, 1)
 			if value_read!=previous_value_written:
-				print "value_read (" + hex(value_read, 1) + ") != value_written (" + hex(previous_value_written, 1) + ")"
+				total_errors += 1
+				#BER = float(total_errors)/total_transfers
+				#print "value_read (" + hex(value_read, 2) + ") != value_written (" + hex(previous_value_written, 2) + ")  BER<=" + eng(BER, "%.1f")
 		previous_value_written = value_written
-		time.sleep(0.1)
+		#time.sleep(0.05)
 		i += 1
+	if (total_errors):
+		BER = float(total_errors)/total_transfers
+		print "BER<=" + eng(BER, "%.1f")
 
 sys.exit(0)
 
@@ -55,8 +103,6 @@ def spi_send_command_address_data(bus, device, command, address, data):
 #	time.sleep(0.001)
 #	spi.writebytes2(to_send)
 #	result = spi.readbytes(4)
-
-size = 100000
 
 command_list = []
 address_list = []
