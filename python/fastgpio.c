1cc0dc65242eab16407b2ae679676b888c207dfc
mza <mza@phys.hawaii.edu>
2020-07-01 10:36:34 -1000

    change required ack_valid quantity to 2

--- python/fastgpio.c
+++ python/fastgpio.c
@@ -383,19 +383,21 @@ typedef struct {
 // pickoff reg_sel/read=3:enable=23:clock50->WARNING=occasionally 9
 // pickoff reg_sel/read=3:enable=30:clock50->WARNING=occasionally 11
 #define MAX_READBACK_CYCLES_WARNING (1)
-#define REQUIRED_ACK_QUANTITY (10)
+#define REQUIRED_ACK_QUANTITY (2)
 
 //char string1[4096] = "";
 //char string2[4096] = "";
 
 u32 set_enable_and_wait_for_ack_valid(half_duplex_bus_object *self) {
+	volatile u32 *set_reg = self->set_reg;
 	volatile u32 *read_port = self->read_port;
 	const u32 ack_valid = self->ack_valid;
+	const u32 enable = self->enable;
 	u32 new_errors = 0;
 	u32 ack_count = 0;
 	u32 value, i;
 	for (i=0; i<MAX_ACK_CYCLES_ERROR; i++) {
-		*self->set_reg = self->enable;
+		*set_reg = enable;
 		value = *read_port;
 		//printf("\n[%08lx] value&ack_valid: %08lx (read_data)", self->transactions, value & ack_valid);
 		if (value & ack_valid) { ack_count++; }
@@ -403,18 +405,21 @@ u32 set_enable_and_wait_for_ack_valid(half_duplex_bus_object *self) {
 	}
 //	if (MAX_ACK_CYCLES_WARNING<i) { sprintf(string2, " ack_valid=%ld(s)", i); strcat(string1, string2); }
 	//if (MAX_ACK_CYCLES_ERROR==i) { new_errors++; }
+	//printf("\nack_count: %ld", ack_count);
 	if (ack_count<REQUIRED_ACK_QUANTITY) { new_errors++; }
 	return new_errors;
 }
 
 u32 clear_enable_and_wait_for_ack_valid(half_duplex_bus_object *self) {
+	volatile u32 *clr_reg = self->clr_reg;
 	volatile u32 *read_port = self->read_port;
 	const u32 ack_valid = self->ack_valid;
+	const u32 enable = self->enable;
 	u32 new_errors = 0;
 	u32 ack_count = 0;
 	u32 value, i;
 	for (i=0; i<MAX_ACK_CYCLES_ERROR; i++) {
-		*self->clr_reg = self->enable;
+		*clr_reg = enable;
 		value = *read_port;
 		//printf("\n[%08lx] value&ack_valid: %08lx (read_data)", self->transactions, value & ack_valid);
 		if (!(value & ack_valid)) { ack_count++; }
