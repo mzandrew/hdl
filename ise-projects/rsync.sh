7a16ddea7a6f370a0a239d9da42b873799d9465c
mzandrew <mza@phys.hawaii.edu>
2021-03-16 09:04:36 -1000

    adding test049 to get an althea revB working on a rpi1 (with 26 pin header)

--- ise-projects/rsync.sh
+++ ise-projects/rsync.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 
-rsync -avm --include="*/" --include="*.bit" --include="*.bin" --exclude="*" ./ mza@2017pi2-idlab:build/hdl/ise-projects/
-rsync -avm --include="*/" --include="*.bit" --include="*.bin" --exclude="*" ./ mza@2018pi2-idlab:build/hdl/ise-projects/
+declare list=""
+list="$list 2013pi1-idlab"
+list="$list 2017pi2-idlab"
+list="$list 2018pi2-idlab"
+
+for each in $list; do
+	echo "$each"
+	rsync -avm --include="*/" --include="*.bit" --include="*.bin" --exclude="*" ./ $each:build/hdl/ise-projects/
+done
 
