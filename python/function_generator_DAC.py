d177bfd90f42819b76f044e590c797d6cb38fd1f
mza <mza@phys.hawaii.edu>
2021-03-18 06:17:25 -1000

    changed amplitude and offset to be in range 0.0-1.0

--- python/function_generator_DAC.py
+++ python/function_generator_DAC.py
@@ -1,22 +1,25 @@
 # written 2021-03-17 by mza
-# last updated 2021-03-17 by mza
+# last updated 2021-03-18 by mza
 
 import althea
 import math
 import sys
 pi = 4.0*math.atan(45.0)
 sampling_frequency = 1.0e9
-DAC_bits = 5
+DAC_bits = 6
 DAC_MAX = 2**DAC_bits
-default_offset = DAC_MAX / 2.0
-default_amplitude = DAC_MAX / 2.0
+default_offset = 0.5
+default_amplitude = 0.5
 default_duty_cycle = 50.0
 default_pulse_duration = 1.0e-9
 
 def prepare_waveform_for_upload_to_DAC(values):
-	if not 0==len(values)%8:
-		print("must be multiple of 8")
-		sys.exit(1)
+	extra = len(values) % 8
+	if not 0==extra:
+		for i in range(extra):
+			values.pop()
+		#print("must be multiple of 8")
+		#sys.exit(1)
 	#print("len(values) = " + str(len(values)))
 	waveform = []
 	for i in range(len(values)):
@@ -39,6 +42,8 @@ def prepare_sine_waveform_for_upload_to_DAC(frequency, amplitude=default_amplitu
 	d = pi/number_of_samples
 	values = [ 0 for a in range(number_of_samples) ]
 	#values = [ 0 for a in range(2**14) ]
+	amplitude *= DAC_MAX
+	offset *= DAC_MAX
 	for i in range(number_of_samples):
 		values[i] = int(offset+amplitude*math.sin(d*i))
 	return prepare_waveform_for_upload_to_DAC(values)
@@ -49,6 +54,8 @@ def prepare_square_waveform_for_upload_to_DAC(frequency, amplitude=default_ampli
 	number_of_samples = int(number_of_samples)
 	number_of_samples_on = duty_cycle * number_of_samples / 100.0
 	number_of_samples_on = int(number_of_samples_on)
+	amplitude *= DAC_MAX
+	offset *= DAC_MAX
 	values = [ int(offset) for a in range(number_of_samples) ]
 	for i in range(number_of_samples_on):
 		values[i] = int(offset+amplitude)
@@ -61,6 +68,8 @@ def prepare_pulse_waveform_for_upload_to_DAC(frequency, amplitude=default_amplit
 	number_of_samples_for_pulse = 1.0e-9 / pulse_duration
 	number_of_samples_for_pulse = int(number_of_samples_for_pulse)
 	#print(str(number_of_samples_for_pulse))
+	amplitude *= DAC_MAX
+	offset *= DAC_MAX
 	values = [ int(offset) for a in range(number_of_samples) ]
 	for i in range(number_of_samples_for_pulse):
 		values[i] = int(offset+amplitude)
@@ -79,6 +88,8 @@ def prepare_sawtooth_waveform_for_upload_to_DAC(frequency, amplitude=default_amp
 	#print(str(number_of_samples_for_falling))
 	values = [ 0 for a in range(number_of_samples) ]
 	peak = offset
+	amplitude *= DAC_MAX
+	offset *= DAC_MAX
 	if number_of_samples_for_rising:
 		for i in range(number_of_samples_for_rising):
 			values[i] = int(offset+amplitude*i/number_of_samples_for_rising)
@@ -89,21 +100,32 @@ def prepare_sawtooth_waveform_for_upload_to_DAC(frequency, amplitude=default_amp
 			values[i] = int(peak-amplitude*(i-number_of_samples_for_rising)/number_of_samples_for_falling)
 	return prepare_waveform_for_upload_to_DAC(values)
 
+def clear_DAC_waveform():
+	values = [ 0 for a in range(2**14) ]
+	althea.write_data_to_pollable_memory_on_half_duplex_bus(0, prepare_waveform_for_upload_to_DAC(values))
+
 def test_function_generator_DAC():
+	clear_DAC_waveform()
 	everything = []
-	waveform = prepare_sine_waveform_for_upload_to_DAC(1.0e6, 16.0, 16.0)
+	f = 1.0e7
+	waveform = prepare_sine_waveform_for_upload_to_DAC(f, 0.5, 0.5)
 	everything.extend(waveform)
 	everything.extend(waveform)
-	waveform = prepare_pulse_waveform_for_upload_to_DAC(1.0e6, 12.0, 4.0, 1.0e-9)
+	everything.extend(waveform)
+	waveform = prepare_pulse_waveform_for_upload_to_DAC(f, 0.375, 0.125, 1.0e-9)
 	everything.extend(waveform)
 	everything.extend(waveform)
-	waveform = prepare_sawtooth_waveform_for_upload_to_DAC(1.0e6, 14.0, 18.0, 25.0)
+	everything.extend(waveform)
+	waveform = prepare_sawtooth_waveform_for_upload_to_DAC(f, 0.4375, 0.5625, 25.0)
 	everything.extend(waveform)
 	everything.extend(waveform)
-	waveform = prepare_square_waveform_for_upload_to_DAC(1.0e6, 10.0, 4.0, 42.0)
+	everything.extend(waveform)
+	waveform = prepare_square_waveform_for_upload_to_DAC(f, 0.3125, 0.125, 42.0)
 	everything.extend(waveform)
 	everything.extend(waveform)
-	waveform = prepare_sine_waveform_for_upload_to_DAC(1.0e6, 8.0, 16.0)
+	everything.extend(waveform)
+	waveform = prepare_sine_waveform_for_upload_to_DAC(f, 0.25, 0.5)
+	everything.extend(waveform)
 	everything.extend(waveform)
 	everything.extend(waveform)
 	#print("len(everything) = " + str(len(everything)))
