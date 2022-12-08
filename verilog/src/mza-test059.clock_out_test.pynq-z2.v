55b6baf22a239d9f6a1ec0e51d0156ea2a091aa2
mza <mza@phys.hawaii.edu>
2022-12-08 09:21:36 -1000

    attempts to take data in at the full 1.27 GHz line-rate on a regular IO

--- verilog/src/mza-test059.clock_out_test.pynq-z2.v
+++ verilog/src/mza-test059.clock_out_test.pynq-z2.v
@@ -1,13 +1,16 @@
 `timescale 1ns / 1ps
 
 // written 2022-11-16 by mza
-// last updated 2022-12-06 by mza
+// last updated 2022-12-08 by mza
 
 module clock_out_test #(
 	parameter clock_select = 0
 ) (
-	input sysclk, // 125 MHz, comes from RTL8211 (ethernet) via 50 MHz osc
+//	input sysclk, // 125 MHz, comes from RTL8211 (ethernet) via 50 MHz osc
 	input [5:4] ja, // 127.216 MHz, comes from PMODA
+	input hdmi_rx_clk_p, // dummy for 1.27216 GHz clock from gulfstream
+	input hdmi_rx_cec, // dummy data
+	output rpio_03_r, // dummy output
 	output hdmi_tx_clk_p,
 	output hdmi_tx_clk_n,
 	output rpio_02_r,
@@ -24,5 +27,10 @@ module clock_out_test #(
 	end
 	OBUFDS (.I(clock), .O(hdmi_tx_clk_p), .OB(hdmi_tx_clk_n));
 	assign rpio_02_r = clock;
+	reg thing = 0;
+	always @(posedge hdmi_rx_clk_p) begin
+		thing <= hdmi_rx_cec;
+	end
+	assign rpio_03_r = thing;
 endmodule
 
