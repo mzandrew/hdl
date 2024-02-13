cba860e82d3173baef6ee1b66cde83ceae2de594
mza <mza@phys.hawaii.edu>
2024-02-13 12:34:47 -1000

    tries to conform to the raspbian bookworm way of doing things

--- python/fastgpio/setup.py
+++ python/fastgpio/setup.py
@@ -2,28 +2,22 @@
 
 # written 2020-06-05 by mza
 # based on example code from https://realpython.com/build-python-c-extension-module/
-# last updated 2020-06-05 by mza
+# last updated 2024-02-13 by mza
 
 from distutils.core import setup, Extension
 
 def main():
-	setup(
-		name="print_string",
-		version="1.0.0",
-		description="python interface for printf",
-		author="mza",
-		author_email="mza@scammerz.gmail.com",
-		ext_modules=[Extension("print_string", ["print_string.c"])])
 	setup(
 		name="fastgpio",
 		version="1.0.0",
 		description="python interface for fast gpio interface",
 		author="mza",
 		author_email="mza@scammerz.gmail.com",
-		ext_modules=[Extension("fastgpio", ["fastgpio.c", "../contrib/DebugInfoWarningError.c"],
+		zip_safe = False,
+		ext_modules=[Extension("fastgpio", ["fastgpio.c", "../../contrib/DebugInfoWarningError.c"],
 			extra_compile_args=['-Wno-missing-braces', '-std=gnu99'],
-			include_dirs=['/opt/vc/include', '../contrib'],
-			library_dirs=['/opt/vc/lib', '../contrib'],
+			include_dirs=['/opt/vc/include', '../../contrib'],
+			library_dirs=['/opt/vc/lib', '../../contrib'],
 			libraries=['bcm_host']
 		)])
 
