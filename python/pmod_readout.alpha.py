dc9608daf93699a8d674c12559d6edc6a5e39344
mza <mza@phys.hawaii.edu>
2024-04-10 13:18:18 -1000

    changes output format again

--- python/pmod_readout.alpha.py
+++ python/pmod_readout.alpha.py
@@ -18,6 +18,8 @@ all_gpios = [ 2, 3, 7, 8, 9, 10, 11 ]
 nybble_gpios = [ 11, 9, 10, 8 ]
 
 #header_description_bytes = [ "AL", "FA", "ASICID", "finetime", "coarse4", "coarse3", "coarse2", "coarse1", "trigger1", "trigger0", "aftertrigger", "lookback", "samplestoread", "startingsample", "missedtriggers", "status" ]
+HEADER_LENGTH_WORDS = 8
+#header_length_nybbles = HEADER_LENGTH_WORDS * 4
 #header_description_words = [ "ALFA", "IdFi", "cs43", "cs21", "tg10", "SaLo", "StSt", "MiSt" ]
 
 bus = []
@@ -33,6 +35,7 @@ data_nybbles = []
 #alignment_nybbles = ( 0xa, 0x1, 0xf, 0xa )
 string = ""
 datafile = open(filename, "w+")
+ALFA_OMGA_counter = 0
 
 def write_strings_and_empty_buffer():
 	global data_string
@@ -49,6 +52,7 @@ def handle_strobe():
 	global data_string
 	global data_nybbles
 	global string
+	global ALFA_OMGA_counter
 	data = 0
 	count += 1
 	nybble_counter += 1
@@ -61,15 +65,21 @@ def handle_strobe():
 	if 0==nybble_counter%4:
 		match = re.search("a1fa", string)
 		if match:
+			ALFA_OMGA_counter = 0
 			print("")
 			#write_strings_and_empty_buffer()
 		print(string, end=" ")
+#		if HEADER_LENGTH_WORDS-1==ALFA_OMGA_counter:
+#			print("")
+		if 0==(ALFA_OMGA_counter-HEADER_LENGTH_WORDS+1)%16:
+			print("")
 		data_string.append(string)
 		match = re.search("0e6a", string)
 		if match:
 			print("")
 			write_strings_and_empty_buffer()
 		string = ""
+		ALFA_OMGA_counter += 1
 
 if __name__ == "__main__":
 	pmod_acknowledge.value = 1
