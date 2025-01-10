9adfa7b5fcce5774e84e19bf069481665ea00331
mza <mza@phys.hawaii.edu>
2025-01-09 15:08:42 -1000

    drives outputs that were otherwise undriven

--- verilog/src/mza-test007.7-segment-driver.v
+++ verilog/src/mza-test007.7-segment-driver.v
@@ -1,22 +1,25 @@
 // written 2018-07-26 by mza
 // based on mza-test006.7-segment-driver.v
 // updated 2020-06-01 by mza
-// last updated 2021-02-04 by mza
+// last updated 2025-01-09 by mza
 
 `define icestick
 `include "lib/segmented_display_driver.v"
 
 module top(
 	input CLK,
-	output reg LED1 = 0,
-	output reg LED2 = 0,
-	output reg LED3 = 0,
-	output reg LED4 = 0,
-	output reg LED5 = 0,
+	input DTRn, RTSn, RX, IR_RX,
+	output DCDn, DSRn, CTSn, TX, IR_TX, IR_SD,
 	output J1_3, J1_4, J1_5, J1_6, J1_7, J1_8, J1_9, J1_10,
 	output J2_1, J2_2, J2_3, J2_4, J2_7, J2_8, J2_9, J2_10,
-	output J3_3, J3_4, J3_5, J3_6, J3_7, J3_8, J3_9, J3_10
+	output J3_3, J3_4, J3_5, J3_6, J3_7, J3_8, J3_9, J3_10,
+	output reg LED5, LED4, LED3, LED2, LED1
 );
+	assign TX = RX; assign DCDn = 1'b1; assign DSRn = 1'b1; assign CTSn = 1'b1;
+	assign IR_TX = IR_RX; assign IR_SD = 1'b1; // IR_SD = shut down
+	assign J1_3 = 1'b0; assign J1_4 = 1'b0; assign J1_5 = 1'b0; assign J1_6 = 1'b0; assign J1_7 = 1'b0; assign J1_8 = 1'b0; assign J1_9 = 1'b0; assign J1_10 = 1'b0;
+	assign J2_1 = 1'b0; assign J2_2 = 1'b0; assign J2_3 = 1'b0; assign J2_4 = 1'b0; assign J2_7 = 1'b0; assign J2_8 = 1'b0; assign J2_9 = 1'b0; assign J2_10 = 1'b0;
+	assign J3_3 = 1'b0; assign J3_4 = 1'b0; assign J3_5 = 1'b0; assign J3_6 = 1'b0; assign J3_7 = 1'b0; assign J3_8 = 1'b0; assign J3_9 = 1'b0; assign J3_10 = 1'b0;
 	wire [6:0] segment;
 	assign J1_4  = segment[0];
 	assign J3_8  = segment[1];
